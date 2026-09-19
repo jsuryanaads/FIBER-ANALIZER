0ebd8254c66611d618f912d263337c37d198076f
We DESIGN <jsuryanaads@gmail.com>
2026-09-19 19:04:24 +0700

    feat: initialize standalone WebVIEW Generator with Android template

---
+++ template/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WebVIEW-Generator-App"
+include(":app")
